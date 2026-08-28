1fe54c1b5e0bf8b183e520cd3e2a8d2d1fdd9267
Ak3no-exe <matteosarakha@gmail.com>
2026-08-28 14:25:56 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'sweet-control-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Réseau d'abord (pour toujours avoir les données à jour), avec repli sur le cache hors-ligne
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
